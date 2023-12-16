adb60eeee4fb0cfd8c0c43993ba3de027449f8c1
mariano.lopez <mariano.lopez@travelxchange.com>
2023-12-16 17:48:38 -0300

    chore: examples

---
+++ js/variables/app.js
@@ -0,0 +1,17 @@
+/* 
+Podemos correr este código directamente en la consola de nuestro
+visual studio code, para ver el resultado de las variables
+*/
+
+// Variables
+
+let nombre = "Juan";
+console.log(nombre);
+
+// Constantes
+
+const apellido = "Perez";
+console.log(apellido);
+
+apellido = "Lara";
+console.log(apellido); // Error porque no se puede modificar una constante
