eda73a50053dbdaa256b451925e3830118dde5e4
mariano.lopez <mariano.lopez@travelxchange.com>
2023-12-16 18:10:55 -0300

    chore: operators

---
+++ js/operators/app.js
@@ -0,0 +1,38 @@
+// Operadores Aritméticos
+let suma = 5 + 3;
+console.log("Suma:", suma);
+
+let resta = 10 - 5;
+console.log("Resta:", resta);
+
+let multiplicacion = 4 * 2;
+console.log("Multiplicación:", multiplicacion);
+
+let division = 20 / 4;
+console.log("División:", division);
+
+let resto = 7 % 3;
+console.log("Resto:", resto);
+
+// Operadores de Asignación
+let numero = 10;
+numero += 5;
+console.log("Número (+= 5):", numero);
+
+// Operadores de Comparación
+let esIgual = 5 === 5;
+console.log("Es 5 igual a 5:", esIgual);
+
+// Operadores Lógicos
+let edad = 30;
+let esMayor = edad > 18 && edad < 65;
+console.log("Es mayor de 18 y menor de 65:", esMayor);
+
+// Operador Ternario
+let esMayorDeEdad = edad > 18 ? "Adulto" : "Menor";
+console.log("Resultado operador ternario (edad):", esMayorDeEdad);
+
+// Literales de Plantilla (Template Literals)
+let nombre = "Alice";
+let saludo = `Hola, ${nombre}! ¿Cómo estás?`;
+console.log("Saludo:", saludo);
