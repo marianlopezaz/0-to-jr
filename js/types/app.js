e28cbd7c4ee3651b1ecdd9ea34400fcce3cf7ccc
mariano.lopez <mariano.lopez@travelxchange.com>
2023-12-16 18:11:31 -0300

    chore: types

---
+++ js/types/app.js
@@ -0,0 +1,35 @@
+// Tipos de Datos
+
+// Números
+let precio = 99.99;
+console.log("Precio:", precio);
+
+// Cadenas de Texto (Strings)
+let ciudad = "Madrid";
+console.log("Ciudad:", ciudad);
+
+// Booleanos
+let esVerdad = true;
+console.log("Es verdad:", esVerdad);
+
+// Undefined
+let variableSinValor;
+console.log("Variable sin valor:", variableSinValor);
+
+// Null
+let valorNulo = null;
+console.log("Valor nulo:", valorNulo);
+
+// Objetos
+let persona = { nombre: "Alice", edad: 25 };
+console.log("Objeto persona:", persona);
+
+// Arrays
+let numeros = [1, 2, 3, 4, 5];
+console.log("Array de números:", numeros);
+
+// Funciones
+function saludoFuncion(nombre) {
+    return `Hola, ${nombre}!`;
+}
+console.log('Llamada a función saludo:', saludoFuncion('Bob'));
